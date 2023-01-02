112f6266178d52cd950d66b8e1ccb15040062210
Kevin <itsksrof@gmail.com>
2023-01-02 09:52:40 +0100

    (perf): added https://github.com/ in the format string

--- zsh/zsh.lua
+++ zsh/zsh.lua
@@ -2,45 +2,45 @@
 -- which will be used to change the user shell.
 -- 
 -- TODO: Improve this to find if the package is installed
-function install_util_linux()
+function InstallUtilLinux()
     os.execute("sudo dnf install util-linux")
 end
 
 -- Check if zsh is installed, if not install it.
-function install_zsh()
+function InstallZsh()
     if os.execute("zsh") == nil then
         os.execute("sudo dnf install zsh")
     end
 end
 
 -- Set zsh as the default shell for the user.
-function default_zsh()
+function DefaultZsh()
     os.execute("chsh -s $(which zsh)")
 end
 
 -- Check if oh-my-zsh is installed, if not install it.
-function install_omz(url)
+function InstallOhMyZsh(url)
     if os.execute("omz") == nil then
         os.execute(string.format('sh -c "$(curl -fsSL %s)"', url))
     end
 end
 
 -- Install oh-my-zsh plugins.
-function install_plugins(plugins)
+function InstallPlugins(plugins)
     for _, value in ipairs(plugins) do
-        os.execute(string.format("git clone %s", value))
+        os.execute(string.format("git clone https://github.com/%s", value))
     end
 end
 
 -- Install oh-my-zsh themes.
-function install_themes(themes, dest)
+function InstallThemes(themes, dest)
     for _, value in ipairs(themes) do
-        os.execute(string.format("git clone %s %s", value, dest))
+        os.execute(string.format("git clone https://github.com/%s %s", value, dest))
     end
 end
 
 -- Copy the installed oh-my-zsh themes to dest folder.
-function copy_themes(sources, dest)
+function CopyThemes(sources, dest)
     for _, value in ipairs(sources) do
         os.execute(
             string.format("cp %s/%s %s", value,
@@ -51,7 +51,7 @@ end
 
 -- Check if there is already a .zshrc file in the system, if there is remove it.
 -- Then copy the .zshrc file from src to dest.
-function copy_config(src, dest)
+function CopyConfig(src, dest)
     if io.open(dest) ~= nil then
         os.remove(dest)
     else
