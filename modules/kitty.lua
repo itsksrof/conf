f2f29c0f68172bdd5d67548cda2e5c80cbd5cb33
Kevin <itsksrof@gmail.com>
2023-01-02 15:25:42 +0100

    (perf): removed kitty.lua neovim.lua and ulauncher.lua

--- modules/kitty.lua
+++
@@ -1,4 +0,0 @@
--- Copy the kitty config folder from the repository to the system.
-function CopyKittyDir(src, dest)
-    os.execute(string.format("cp -r %s %s", src, dest))
-end
--- modules/neovim.lua
+++
@@ -1,4 +0,0 @@
--- Copy the neovim config folder from the repository to the system.
-function CopyNeovimDir(src, dest)
-    os.execute(string.format("cp -r %s %s", src, dest))
-end
