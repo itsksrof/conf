b053f68c250c99334fafbb72194d76dcf78b5d61
Kevin <itsksrof@gmail.com>
2023-01-02 09:53:05 +0100

    (perf): removed https://github.com/ from plugins/themes

--- setup.lua
+++ setup.lua
@@ -1,20 +1,34 @@
 require("zsh.zsh")
 
--- Install and configure zsh and oh-my-zsh
-install_util_linux()
-install_zsh()
-default_zsh()
-install_omz("https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh")
-install_plugins({
-    "https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z",
-    "https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting",
-    "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions",
-    "https://github.com/jirutka/zsh-shift-select.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-shift-select",
+-- Provides chsh.
+InstallUtilLinux()
+
+-- Provides zsh.
+InstallZsh()
+
+-- Sets zsh as default user shell.
+DefaultZsh()
+
+-- Installs oh-my-zsh.
+InstallOhMyZsh("https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh")
+
+-- Install oh-my-zsh plugins.
+InstallPlugins({
+    "agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z",
+    "zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting",
+    "zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions",
+    "jirutka/zsh-shift-select.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-shift-select",
 })
-install_themes({
-    "https://github.com/ergenekonyigit/lambda-gitster.git",
+
+-- Install oh-my-zsh themes.
+InstallThemes({
+    "ergenekonyigit/lambda-gitster.git",
 }, "~/")
-copy_themes({
+
+-- Copy oh-my-zsh themes.
+CopyThemes({
     "~/lambda-gitster",
 }, "~/.oh-my-zsh/custom/themes/")
-copy_config("./.zshrc", "~/.zshrc")
+
+-- Copy zsh config.
+CopyConfig("./.zshrc", "~/.zshrc")
